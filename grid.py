5450c8f79399f61d1524d50144dd02a0f059c013
Maxence-villet <maxence.villetschoumaker@gmail.com>
2024-10-16 10:32:05 +0200

    update de l'interface du jeu & bug fix

--- grid.py
+++ grid.py
@@ -1,5 +1,5 @@
 class Grid:
-    def __init__(self):
+    def __init__(self, size):
         """
         Constructeur de la classe Grid.
 
@@ -11,9 +11,11 @@ class Grid:
         --------
             None
         """
-        self.size = 4
+        self.size = size
         self.data = [[0 for _ in range(self.size)] for _ in range(self.size)]  # Initialisation de la grille dans le constructeur
-
+        self.ship_sizes = {'porte-avions': 5, 'croiseur': 4, 'contre-torpilleur': 3, 'sous-marin': 3, 'torpilleur': 2} # Dictionnaire pour associer chaque type de bateau à sa taille
+        self.placed_ships = []
+        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     def display_grid(self):
         """
         Affiche la grille à l'écran.
@@ -26,15 +28,15 @@ class Grid:
         --------
             None
         """
-        i = 1
+        i = 1 # Incrémentation des colonnes
         
-        # Affichage Alphabet
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
         print("   ", end="")
         for letter in range(0,self.size):
-            print(alphabet[letter], end=" ")
+            print(self.alphabet[letter], end=" ")
         print()
 
+        # Affichage de la grille
         for row in self.data:
             if i <10:
                 print(i, end="  ")
@@ -43,9 +45,32 @@ class Grid:
             for cell in row:
                 print(cell, end=" ")
             print()
-            i+=1
+            i+=1 # Incrémentation des colonnes
 
-    def place_ship(self, ship_type, row, col, orientation):
+    def set_ship_size(self, ship_type):
+        """
+        Retire un bateau de la liste des bateaux disponibles et met à jour la liste des bateaux placés.
+
+        Paramètres:
+        -----------
+            ship_type (sting): Le type de bateau à placer.
+
+        Retourne:
+        ---------
+            bool: True si le bateau a été placé, False sinon.
+        """
+
+        if ship_type in self.ship_sizes:
+            if ship_type not in self.placed_ships:
+                self.placed_ships.append(ship_type)
+                return True
+            else:
+                print("Ce bateau a déjà été placé.")
+        else:
+            print("Type de bateau inconnu.")
+        return False
+
+    def place_ship(self, ship_type, row, col_alpha, orientation):
         """
         Place un bateau sur la grille.
 
@@ -60,12 +85,21 @@ class Grid:
         ---------
             True si le placement est valide, False sinon.
         """
+        row-=1
 
-        # Dictionnaire pour associer chaque type de bateau à sa taille
-        ship_sizes = {'porte-avions': 5, 'croiseur': 4, 'contre-torpilleur': 3, 'sous-marin': 3, 'torpilleur': 2}
+        col:int
+        isContain = False
+        for i in range(0, len(self.alphabet)):
+             if col_alpha == self.alphabet[i]:
+                 col = i
+                 isContain = True
+             else:
+                 continue
+        if isContain == False:
+            return False
 
         # Récupérer la taille du bateau
-        ship_size = ship_sizes[ship_type]
+        ship_size = self.ship_sizes[ship_type]
 
         # Vérifier si le placement est valide
         if orientation == 'H':
@@ -83,10 +117,27 @@ class Grid:
 
         # Si le placement est valide, mettre à jour la grille
         if orientation == 'H':
+            self.set_ship_size(ship_type)
             for i in range(ship_size):
                 self.data[row][col + i] = 1  # 1 représente une case occupée par un bateau
         else:
+            self.set_ship_size(ship_type)
             for i in range(ship_size):
                 self.data[row + i][col] = 1
 
         return True
+    
+    def get_ships(self):
+        """
+        Cette fonction retourne la liste des bateaux
+
+        Paramètre:
+        ----------
+            None
+
+        Retourne:
+        ---------
+            Hashmap: la liste des bateaux
+        """
+        return self.ship_sizes
+    
--- main.py
+++ main.py
@@ -1,8 +1,50 @@
 from grid import Grid;
+import time
+import os
 
 
 def main():
-    g = Grid()
-    # g.place_ship('torpilleur', 1, 2, 'H')
-    g.display_grid()
+    g = Grid(10)
+    #g.place_ship('contre-torpilleur', 9, 2, 'V')
+
+    i = 0
+    
+    types_bateaux = list(g.ship_sizes.keys())
+    print(types_bateaux)
+
+    # Boucle pour placer tous les bateaux
+    while types_bateaux:
+        os.system('cls||clear')
+        g.display_grid()
+        # Afficher les types de bateaux restants à placer
+        print("Bateaux restants à placer :", types_bateaux)
+
+        # Demander au joueur de choisir un type de bateau
+        type_bateau = input("Choisissez un type de bateau à placer : ")
+
+        # Vérifier si le type de bateau est valide
+        if type_bateau not in types_bateaux:
+            print("Type de bateau invalide. Veuillez réessayer.")
+            continue
+
+        # Demander les coordonnées et l'orientation du bateau
+        ligne = int(input("Entrez la ligne (numéro) : "))
+        colonne = input("Entrez la colonne (lettre) : ")
+        orientation = input("Entrez l'orientation (horizontale ou verticale) : ")
+
+        # Appeler la fonction place_ship() pour placer le bateau
+        if g.place_ship(type_bateau, ligne, colonne, orientation):
+            # Si le placement est réussi, retirer le type de bateau de la liste
+            types_bateaux.remove(type_bateau)
+        else:
+            print("Placement impossible. Veuillez réessayer.")
+
+    print("Tous les bateaux ont été placés !")
+
+
+    # Affichage du jeu
+    while i < 20:
+        
+        i+=2
 main()
+
